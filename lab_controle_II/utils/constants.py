80269049990636d7e69af0dd32dcb34d246200d3
Kauan Simplício <kauan-simplicio@poli.ufrj.br>
2023-12-22 04:48:47 -0300

    Update constants.py

--- lab_controle_II/utils/constants.py
+++ lab_controle_II/utils/constants.py
@@ -1,5 +1,9 @@
-XLSX_PATH = r'C:\Users\kauan\Documents\GitHub\PYTHON\lab_controle_II\data\experiment_data.xlsx'
-CSV_PATH = r'C:\Users\kauan\Documents\GitHub\PYTHON\lab_controle_II\data\oscilloscope_data.csv'
+import os
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(CURRENT_DIR, '..', 'data')
+
+XLSX_PATH = os.path.join(DATA_DIR, 'experiment_data.xlsx')
+CSV_PATH = os.path.join(DATA_DIR, 'oscilloscope_data.csv')
 ARMOR_VOLTAGE = 'Tensão'
 TACOMETER_VOLTAGE = 'Tacômetro'
 ENGINE_SPEED = 'Rotação'
